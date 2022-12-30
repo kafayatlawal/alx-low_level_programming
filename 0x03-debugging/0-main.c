f62617e108e34bc422da4a35f28f6a366e0e2e77
kafayatlawal <kafayatlawal@github.com>
2022-12-29 16:34:19 -0800

    Task O

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: O
+ */
+
+int main(void)
+{
+int i;
+
+printf("infinite loop incoming :(\n");
+
+i = O;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("infinite loop avoided!\\o\/\n");
+
+return (O);
